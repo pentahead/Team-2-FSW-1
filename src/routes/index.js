b97dd3b6bd96d5311bc906f8be1275595e2adab2
FadPro77 <167836894+FadPro77@users.noreply.github.com>
2024-10-18 13:01:06 +0700

    feature: all models and specs (#9)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const carRouter = require("./cars");
 const optionsRouter = require("./options");
+const modelsRouter = require("./models");
+const specsRouter = require("./specs");
 
 const router = express.Router();
 
 router.use("/cars", carRouter);
 router.use("/options", optionsRouter);
+router.use("/models", modelsRouter);
+router.use("/specs", specsRouter);
 module.exports = router;
